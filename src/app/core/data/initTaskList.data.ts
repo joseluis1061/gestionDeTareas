705cbfca6abcf76a02a8d06057f1faa7118a3ca2
José Luis Zúñiga <jose@tangibledesign.co>
2024-09-29 13:56:09 -0500

    Separate dummy data listTask

---
+++ src/app/core/data/initTaskList.data.ts
@@ -0,0 +1,94 @@
+export const initTaskList = [
+  {
+    id: 1,
+    taskName: "Tarea 1",
+    complete: false,
+    taskDate: "2024-09-27",
+    persons: [
+      {
+        id: 1,
+        personName: "Juan Pérez",
+        age: 30,
+        personSkills: ["Angular", "TypeScript", "CSS"]
+      },
+      {
+        id: 2,
+        personName: "María Gómez",
+        age: 25,
+        personSkills: ["Java", "Spring Boot", "Hibernate"]
+      }
+    ]
+  },
+  {
+    id: 2,
+    taskName: "Tarea 2",
+    complete: true,
+    taskDate: "2024-09-29",
+    persons: [
+      {
+        id: 3,
+        personName: "Carlos Rodríguez",
+        age: 35,
+        personSkills: ["Python", "Django", "REST APIs"]
+      },
+      {
+        id: 4,
+        personName: "Ana Martínez",
+        age: 28,
+        personSkills: ["JavaScript", "React", "Redux"]
+      },
+      {
+        id: 5,
+        personName: "Luis Fernández",
+        age: 40,
+        personSkills: ["C#", ".NET", "Azure"]
+      }
+    ]
+  },
+  {
+    id: 3,
+    taskName: "Tarea 3",
+    complete: false,
+    taskDate: "2024-10-10",
+    persons: [
+      {
+        id: 6,
+        personName: "Sofía López",
+        age: 22,
+        personSkills: ["HTML", "CSS", "JavaScript"]
+      },
+      {
+        id: 7,
+        personName: "Miguel Torres",
+        age: 32,
+        personSkills: ["Node.js", "Express", "MongoDB"]
+      }
+    ]
+  },
+  {
+    id: 4,
+    taskName: "Tarea 4",
+    complete: true,
+    taskDate: "2024-10-15",
+    persons: [
+      {
+        id: 8,
+        personName: "Laura Ramírez",
+        age: 27,
+        personSkills: ["Vue.js", "Vuex", "Vuetify"]
+      }
+    ]
+  },
+  {
+    id: 5,
+    taskName: "Tarea 5",
+    complete: false,
+    taskDate: "2024-10-21",
+    persons: [{
+      id: 6,
+      personName: "Sofía López",
+      age: 22,
+      personSkills: ["HTML", "CSS", "JavaScript"]
+    }]
+  }
+];
--- src/app/core/service/task.service.ts
+++ src/app/core/service/task.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient }from '@angular/common/http';
 import { ITask } from 'src/app/models/task.model';
 import { BehaviorSubject, delay, Observable, of } from 'rxjs';
+import { initTaskList } from '../data/initTaskList.data';
 @Injectable({
   providedIn: 'root'
 })
@@ -19,101 +20,7 @@ export class TaskService {
 
 
   getTasks(): Observable<ITask[]> {
-    const data =       [
-      {
-        id: 1,
-        taskName: "Tarea 1",
-        complete: false,
-        taskDate: "2024-09-27",
-        persons: [
-          {
-            id: 1,
-            personName: "Juan Pérez",
-            age: 30,
-            personSkills: ["Angular", "TypeScript", "CSS"]
-          },
-          {
-            id: 2,
-            personName: "María Gómez",
-            age: 25,
-            personSkills: ["Java", "Spring Boot", "Hibernate"]
-          }
-        ]
-      },
-      {
-        id: 2,
-        taskName: "Tarea 2",
-        complete: true,
-        taskDate: "2024-09-29",
-        persons: [
-          {
-            id: 3,
-            personName: "Carlos Rodríguez",
-            age: 35,
-            personSkills: ["Python", "Django", "REST APIs"]
-          },
-          {
-            id: 4,
-            personName: "Ana Martínez",
-            age: 28,
-            personSkills: ["JavaScript", "React", "Redux"]
-          },
-          {
-            id: 5,
-            personName: "Luis Fernández",
-            age: 40,
-            personSkills: ["C#", ".NET", "Azure"]
-          }
-        ]
-      },
-      {
-        id: 3,
-        taskName: "Tarea 3",
-        complete: false,
-        taskDate: "2024-10-10",
-        persons: [
-          {
-            id: 6,
-            personName: "Sofía López",
-            age: 22,
-            personSkills: ["HTML", "CSS", "JavaScript"]
-          },
-          {
-            id: 7,
-            personName: "Miguel Torres",
-            age: 32,
-            personSkills: ["Node.js", "Express", "MongoDB"]
-          }
-        ]
-      },
-      {
-        id: 4,
-        taskName: "Tarea 4",
-        complete: true,
-        taskDate: "2024-10-15",
-        persons: [
-          {
-            id: 8,
-            personName: "Laura Ramírez",
-            age: 27,
-            personSkills: ["Vue.js", "Vuex", "Vuetify"]
-          }
-        ]
-      },
-      {
-        id: 5,
-        taskName: "Tarea 5",
-        complete: false,
-        taskDate: "2024-10-21",
-        persons: [{
-          id: 6,
-          personName: "Sofía López",
-          age: 22,
-          personSkills: ["HTML", "CSS", "JavaScript"]
-        }]
-      }
-    ];
-    return of(data).pipe(delay(1500))
+    return of(initTaskList).pipe(delay(1500))
   }
 
   createTask(task: ITask): Observable<any> {
